ad7ed01d9371bfc18b546b72a0dd3bbd597f89f0
ai-man-123 <aimanmk1234@gmail.com>
2022-11-05 11:23:35 +0800

    testaiman

---
+++ config.js
@@ -0,0 +1,80 @@
+/** 
+
+Yg Mau Sv Nomerku?
+
+wa.me/6285875158363
+
+Minimal Kasih Cr kalo mau reupload😡
+Create By Dika Ardnt.
+Recode By Naze & ( Nama Lu )
+
+Kalau Sampai Ketemu Lagi Creator Bot
+Yg Reupload Tanpa Kasih Cr 
+Sc Akan Saya Encode
+
+Base : Hisoka
+Recode : Naze & ( Nama Lu )
+
+**/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '0e92565522',
+}
+
+// Other
+global.owner = ['60102810046']
+global.ownernomer = "60102810046"
+global.premium = ['60102810046']
+global.packname = 'Sticker By'
+global.author = 'lynxmanbotメ'
+global.sessionName = 'nazedev'  //jangan diganti bro nanti error
+global.jumlha = '999'
+global.jumhal = '100000000000000'
+global.jumlah = '1000000000'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+global.mess = {
+    success: '✅Done',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    errapi: 'Error Mungkin Apikey Tidak Valid!',
+    errmor: 'Error Kesalahan Sistem',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 999999
+}
+global.thumb = fs.readFileSync('./media/image/naze.jpg')
+global.faall = fs.readFileSync('./media/image/fake.jpg')
+global.mygit = 'https://github.com/ai-man-123'
+global.myyt = '-'
+global.myytv = '-'
+global.mygc = "-"
+global.botname = 'Lynxbot'
+global.akulaku = 'Bot By Aiman'
+global.ytname = '-'
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
